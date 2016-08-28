894516106adf59b76d05a65e55b3a9f96ece8350
t-k- <clock126@126.com>
2016-08-28 23:39:54 +0800

    Add a simple script to deploy searchd.

---
+++ scripts/deploy-searchd.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+PROJECT="/home/tk/tksync/proj/search-engine/"
+
+cd "$PROJECT"
+
+echo 'Pull code  ...'
+git pull
+
+echo 'Will rebuild search-engine and deploy in 5 sec ...'
+sleep 5
+
+pushd searchd/
+echo 'Shutdown searchd ...'
+./scripts/respawn.sh kill
+popd
+
+pushd indexer/
+echo 'Unmount index vdisk ...'
+./scripts/vdisk-umount.sh
+popd
+
+echo 'Rebuilding ...'
+make new
+
+pushd indexer/
+echo 'Mount index vdisk ...'
+./scripts/vdisk-mount.sh `whoami` ./tmp
+popd
+
+pushd searchd/
+echo 'Start searchd ...'
+./scripts/respawn.sh ./run/searchd.out -e -i ../indexer/tmp/ &
+popd
+
+echo 'Deploy done.'
